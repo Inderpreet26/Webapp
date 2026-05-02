d890c47bd1eb760a7d0985fc5720b20314c6d29c
Inderpreet Singh Makkar <117035818+Inderpreet26@users.noreply.github.com>
2026-05-02 15:32:34 +0530

    Add Task class with name property and getter

---
+++ src/main/java/com/app/model/Task.java
@@ -0,0 +1,13 @@
+package com.app.model;
+
+public class Task {
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
